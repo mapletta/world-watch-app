0a20cfb79d0b6a306464acb3375be7af51f64afb
mapletta <anggaw0901@gmail.com>
2022-12-30 15:34:10 +0100

    adding setInterval function//real time update

--- clock.js
+++ clock.js
@@ -1,19 +1,22 @@
-//Los Angeles
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+setInterval(function () {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format(
-  "h:mm:ss [<small>] A[</small>]"
-);
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>] A[</small>]"
+  );
+}, 1000);
+setInterval(function () {
+  //Rome
+  let romeElement = document.querySelector("#rome");
+  let romeDateElement = romeElement.querySelector(".date");
+  let romeTimeElement = romeElement.querySelector(".time");
+  let romeTime = moment().tz("Europe/Rome");
 
-//Rome
-let romeElement = document.querySelector("#rome");
-let romeDateElement = romeElement.querySelector(".date");
-let romeTimeElement = romeElement.querySelector(".time");
-let romeTime = moment().tz("Europe/Rome");
-
-romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
-romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>] A[</small>]");
+  romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
+  romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>] A[</small>]");
+}, 1000);
